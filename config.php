ad2d87dfb556bd336fa86bbcbfcdf35859033c27
Michael Li <columbia.cs@gmail.com>
2014-02-26 11:54:38 -0700

    initial commit

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+// phpBB 3.0.x auto-generated configuration file
+// Do not change anything in this file!
+$dbms = 'mysql';
+$dbhost = '';
+$dbport = '';
+$dbname = 'pearls7_floraclassifieds';
+$dbuser = 'pearls7_flora';
+$dbpasswd = 'Ell8isTortoise10';
+$table_prefix = 'phpbb3_';
+$acm_type = 'file';
+$load_extensions = '';
+
+@define('PHPBB_INSTALLED', true);
+// @define('DEBUG', true);
+// @define('DEBUG_EXTRA', true);
